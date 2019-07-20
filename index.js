3b14615b0ef4f4d0bda8f72845e7e1e9aaf32cf0
Cha Yang <chachaiyang3@gmail.com>
2019-07-20 09:47:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ whisper('HELLO');
 function logShout(string){
   console.log(string.toUpperCase());
 }
-logShout('log');
+logShout('hello');
 function logWhisper(string){
-  
+  console.log(string.toLowerCase());
 }
+logWhisper('HELLO');
