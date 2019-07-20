77516d360cf88a77bd409e4fa3cdd4855d4a21e9
Cha Yang <chachaiyang3@gmail.com>
2019-07-20 09:53:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,20 +28,3 @@ function sayHiToGrandma(string){
 sayHiToGrandma('hello');
 sayHiToGrandma('HELLO');
 sayHiToGrandma("I love you");
-
-
-
-
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
